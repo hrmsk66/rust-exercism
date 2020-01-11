a7b41ce21136b62a98672c6ae068d3bc80a41882
hrmsk66 <hrmsk66@gmail.com>
2020-01-12 08:56:50 +0900

    grains 2

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn square(s: u32) -> u64 {
     match s {
-        s if s < 1 || 64 < s => panic!("Square must be between 1 and 64"),
-        _ => 1<<(s - 1)
+        1..=64 => 1<<(s - 1),
+        _ => panic!("Square must be between 1 and 64")
     }
 }
 
 pub fn total() -> u64 {
-    (1..=64).fold(0, |acc, x| acc + square(x))
+    (1..=64).map(square).sum()
 }
