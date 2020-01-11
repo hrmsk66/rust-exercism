21389427a088a3bf27fb33843ca1987087348dc1
hrmsk66 <hrmsk66@gmail.com>
2020-01-11 22:38:42 +0900

    sum-of-multiples 2

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,16 +1,10 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut set = HashSet::new();
-    
-    for &x in factors.iter() {
-        if x == 0 { continue; }
-
-        (x..limit).step_by(x as usize).for_each(|n| {
-            set.insert(n);
-            ()
-        });
-    }
-
-    set.iter().fold(0, |sum, x| sum + x)
+    (1..limit).fold(0, |acc, x| {
+        acc + {
+            match factors.iter().any(|&f| f != 0 && x % f == 0) {
+                true => x,
+                false => 0,
+            }
+        }
+    })
 }
