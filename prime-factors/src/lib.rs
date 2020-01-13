2d38c06d0c6b22a19ed806c63ec7de1407e836d0
hrmsk66 <hrmsk66@gmail.com>
2020-01-12 23:13:45 -0700

    prime-factors 2

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,21 +1,12 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut r = Vec::new();
+    let mut result = vec![];
 
-    let mut curr = n;
-    while 1 < curr {
-        for x in 2..=curr {
-            if curr % x == 0 {
-                r.push(x);
-                curr = curr / x;
-                break
-            }
-
-            if curr == x {
-                r.push(x);
-                return r;
-            }
-        }
+    match(2..=n).find(|x| n % x == 0) {
+        Some(x) => {
+            result.push(x);
+            result.append(&mut factors(n / x));
+        },
+        None => {}
     }
-
-    r
+    result
 }
