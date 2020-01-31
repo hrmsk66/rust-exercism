0a9047b6bbf934709c03a0dc524344c6bb6f57ce
hrmsk66 <hrmsk66@gmail.com>
2020-01-31 23:22:51 +0900

    sieve3

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -2,15 +2,10 @@ use std::collections::HashSet;
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut non_primes: HashSet<u64> = HashSet::new();
-    (2..=upper_bound).filter_map(|x| {
-        if non_primes.contains(&x) {
-            return None;
-        }
-
-        for y in ((x * 2)..=upper_bound).step_by(x as usize) {
+    (2..=upper_bound).filter(|x| {
+        for y in ((x * 2)..=upper_bound).step_by(*x as usize) {
             non_primes.insert(y);
         }
-
-        Some(x)
+        !non_primes.contains(&x)
     }).collect()
 }
