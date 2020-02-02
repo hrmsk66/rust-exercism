c290ea1cadb69c90f2eae51810e979a00b039584
hrmsk66 <hrmsk66@gmail.com>
2020-02-02 17:44:13 +0900

    grade-school2

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,37 +1,29 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
-pub struct School { students: HashMap<u32, Vec<String>> }
+#[derive(Default)]
+pub struct School {
+    grades: BTreeMap<u32, Vec<String>>,
+}
 
 impl School {
     pub fn new() -> School {
-        School { students: HashMap::new() }
+        School {
+            grades: BTreeMap::new(),
+        }
     }
-
     pub fn add(&mut self, grade: u32, student: &str) {
-        if self.students.contains_key(&grade) {
-            self.students.get_mut(&grade).unwrap().push(student.to_string());
-        } else {
-            self.students.insert(grade, vec![student.to_string()]);
-        }
+        self.grades
+            .entry(grade)
+            .or_insert(vec![])
+            .push(String::from(student))
     }
-
     pub fn grades(&self) -> Vec<u32> {
-        let mut r = self.students.keys().map(|x| x.clone()).collect::<Vec<u32>>();
-        r.sort();
-        r
+        self.grades.keys().copied().collect::<Vec<u32>>()
     }
-
-    // If grade returned an `Option<&Vec<String>>`,
-    // the internal implementation would be forced to keep a `Vec<String>` to lend out.
-    // By returning an owned vector instead,
-    // the internal implementation is free to use whatever it chooses.
     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
-        if let Some(g) = self.students.get(&grade) {
-            let mut r = g.clone();
-            r.sort();
-            Some(r)
-        } else {
-            None
-        }
+        self.grades.get(&grade).cloned().map(|mut names| {
+            names.sort();
+            names
+        })
     }
 }
