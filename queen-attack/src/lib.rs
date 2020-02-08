0edfab948205fb2df8edf3233055d1f025473ad8
hrmsk66 <hrmsk66@gmail.com>
2020-02-08 23:26:05 +0900

    queen-attack2

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -8,10 +8,10 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        if rank < 0 || file < 0 || 7 < rank || 7 < file {
-            return None;
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(ChessPosition(rank, file)),
+            _ => None,
         }
-        Some(ChessPosition(rank, file))
     }
 }
 
@@ -23,9 +23,6 @@ impl Queen {
     pub fn can_attack(&self, other: &Queen) -> bool {
         let x = self.position.0 - other.position.0;
         let y = self.position.1 - other.position.1;
-        if x == 0 || y == 0 || i32::abs(x) == i32::abs(y) {
-            return true;
-        }
-        false
+        x == 0 || y == 0 || i32::abs(x) == i32::abs(y) 
     }
 }
