a8500bd9affe6217d72d8bd41385ea5e8fe99f1f
hrmsk66 <hrmsk66@gmail.com>
2020-02-22 00:07:42 +0900

    isogram

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -2,10 +2,5 @@ use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
     let mut set = HashSet::new();
-    for c in candidate.to_ascii_lowercase().chars().filter(|&c| c != ' ' && c != '-') {
-        if !set.insert(c) {
-            return false;
-        }
-    }
-    return true
+    candidate.chars().all(|c| !c.is_ascii_alphabetic() || set.insert(c.to_ascii_lowercase()))
 }
